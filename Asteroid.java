2d9ead04d4e1e4b864f26417f861774f3d7970b2
NPailoor <nik515@uw.edu>
2015-08-22 21:50:03 -0700

    Create Asteroid.java
    Adds in the Asteroid object, based on format of Debris.java

---
+++ Asteroid.java
@@ -0,0 +1,18 @@
+ public class Asteroid {
+ 	
+	Random d20;
+
+	public Hex(Border A, Border B, Border C, Border D, Border E, Border F) {
+		super(A, B, C, D, E, F, null);
+		d20 = new Random();
+	}	
+	
+	public void shipEnter(Ship s) {
+		diceRoll = d20.nextInt(20) + 1;
+		if (diceRoll > 11) {
+			myShip = s;
+		}
+		else {
+			s.damage(diceRoll);
+		}
+}
