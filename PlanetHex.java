5ab664bcf3e63e602d743b53867e1bc07f709a3f
NPailoor <nik515@uw.edu>
2015-08-23 22:42:04 -0700

    Update PlanetHex.java

--- PlanetHex.java
+++ PlanetHex.java
@@ -1,8 +1,16 @@
 public class PlanetHex extends Hex{
 	private Planet myPlanet;
-	public PlanetHex(Border[] b, Ship s, Planet p) {
+	private boolean canBuy;
+	private boolean canSell;
+	public PlanetHex(Border[] b, Ship s, Planet p, boolean canBuy, boolean canSell) {
 		myPlanet = p;
+		this.canBuy = canBuy;
+		this.canSell = canSell;
 		this(b,s);
+   	}
+   	
+   	public PlanetHex(Border[] b, Ship s, Planet p) {
+   		this(b, s, p, false, false);
    	}
 	//Topmost border is A, then working clockwise is B, C, D, E, F
 	public PlanetHex(Border A, Border B, Border C, Border D, Border E, Border F, Ship s, Planet p) {
@@ -29,7 +37,7 @@ public class PlanetHex extends Hex{
 		if (cargo < 0 | cargo > 5) {
 			throw new IllegalArgumentException("Invalid Cargo");
 		} else {
-			return (myPlanet.supply == cargo);
+			return (myPlanet.supply == cargo && this.canBuy);
 		
 		}
 	}
@@ -38,7 +46,7 @@ public class PlanetHex extends Hex{
 		if (cargo < 0 | cargo > 5) {
 			throw new IllegalArgumentException("Invalid Cargo");
 		} else {
-			return (myPlanet.demand == cargo);
+			return (myPlanet.demand == cargo && this.canSell);
 		
 		}
 	}
