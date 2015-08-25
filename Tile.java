b784fdd686d3fb698c05e4527a96b100bfcab13f
NPailoor <nik515@uw.edu>
2015-08-25 16:51:03 -0700

    Update Tile.java
    Elaboration on Tile-Fix comments

--- Tile.java
+++ Tile.java
@@ -1,42 +1,25 @@
 import java.util.*;
 public class Tile {
-  public Hex[] topLeft; // 3 hexs, as in here https://boardgamegeek.com/camo/87972fe60db21770a52d4793e0d67f75599f64c7/687474703a2f2f63662e6765656b646f2d696d616765732e636f6d2f696d616765732f706963323237353337355f6d642e706e67
-  // note: That is a fanmade tile that we don't need to implement.
-  public Hex[] middleLeft; // 4 hexs
-  public Hex[] middle; // 5 hexs
-  public Hex[] middleRight; // 4 hexs
-  public Hex[] bottomRight; // 3 hexs
-  public Hex[] edges; // For the getHex method
-  public int[] tileBoundary; // used to line up tiles;
-  
-  public Tile(Hex[] topLeft, Hex[] middleLeft, Hex[] middle, Hex[] middleRight, Hex[] bottomRight) {
-    this.topLeft = topLeft;
-    if (topLeft.length() != 3) {
-      throw new IllegalStateException("Wrong Tile Geometry");
-    }
-    this.middleLeft = middleLeft;
-    if (middleLeft.length() != 4) {
-      throw new IllegalStateException("Wrong Tile Geometry");
-    }
-    this.middle = middle;
-    if (middle.length() != 5) {
-      throw new IllegalStateException("Wrong Tile Geometry");
-    }
-    this.middleRight = middleRight;
-    if (middleRight.length() != 4) {
-      throw new IllegalStateException("Wrong Tile Geometry");
-    }
-    this.bottomRight = bottomRight;
-    if (bottomRight.length() != 3) {
-      throw new IllegalStateException("Wrong Tile Geometry");
-    }
-    this.edges = [topLeft, middleLeft(0), middleLeft(end), middle(0), middle(end), middleRight(0), middleRight(end), bottomRight(0), bottomRight(end)]; // my Java is a bit rusty, not sure if this line will work as intended
-    for (i = 0; i < edges.length(); i++) {
-      tileBoundary(i) = i;
-    }
+  private Hex[] hexList //Index of Hexes, to be implemented in individual extensions
+  public Border[][] side;  //Six arrays of edges
+  /*  0 -> Delta type sides
+      1 -> O
+      2 -> Square
+      3 -> T
+      4 -> I
+      5 -> Y
+  */
+  public Tile(Border[] Delta, Border[] O, Border[] Square, Border[] T, Border[] I, Border[] Y) {
+    side[0] = Delta;
+    side[1] = O;
+    side[2] = Square;
+    side[3] = T;
+    side[4] = I;
+    side[5] = Y;
   }
   
-  public Hex getHex(Tile requesting) { // this method at least needs to accept the requesting Tile, which in Hex it currently doesn't. I've changed Hex to account for that.
-    return null;
+  public Hex mergeTile(Tile T, index i) {
+    this.side[i] = T.side[i];
   }
+    
 }
