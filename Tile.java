3c841d5fbcecbb32b93955cc46c45f81f016c612
NPailoor <nik515@uw.edu>
2015-08-28 08:57:30 -0700

    Update Tile.java
    Defining toConnect method

--- Tile.java
+++ Tile.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Tile {
   private Hex[] hexList //Index of Hexes, to be implemented in individual extensions
-  public Border[][] side;  //Six arrays of edges
+  public Side[] side;  //Six arrays of edges
   /*  0 -> Delta type sides
       1 -> O
       2 -> Square
@@ -9,8 +9,10 @@ public class Tile {
       4 -> I
       5 -> Y
   */
+  public int orient; //Number of turns counterclockwise; orient = 5 would put the Y side at the top
   public String name;
   public Tile(int orient, String name) {
+    this.orient = orient;
     side[0] = new Side(orient);
     side[1] = new Side(orient + 1);
     side[2] = new Side(orient + 2);
@@ -19,10 +21,30 @@ public class Tile {
     side[5] = new Side(orient + 5);
   }
   
-  public Hex mergeTile(Tile T, int i, int j) {
-    Hex B = this[i][j].spaces[0]; //Draws Hex from corresponding border
-    this.side[i] = T.side[i];
+  //For merging new tile to tile discovered from
+  public Hex mergeTile(Tile T, int i, int j, Board gameBoard) {
+    Hex B = this.side[i].borders[4 - j].spaces[0]; //Draws Hex from corresponding border
+    for (int k = 0; k < 5; k++) {
+      Border temp = T.side[i].borders[4 - k];
+      Hex A = temp.spaces[0];
+      //Fills out "mixxing hex" for new Tile's 
+      this.side[i].borders[k].spaces[1] = A;
+      T.side[i].borders[4 - k] = this.side[i].borders.borders[k];
+    }
     return B;
+  }
+  
+  //for merging new tile to arbitrary other tile
+  public void connect(Tile T, int orient) {
+      int otherDirection = (orient + 3)%6;
+      Side toConnect = T.side[(otherDirection - T.orient)%6];
+      int mySide = (orient - this.orient)%6;;
+    for (int k = 0; k < 5; k++) {
+      Border temp = toConnect.borders[4 - k];
+      Hex A = temp.spaces[0];
+      this.side[mySide].borders[k].spaces[1] = A;
+      toConnect.borders[4 - k] = this.side[i].borders.borders[k];
+    }
   }
   
   public String toString() {
