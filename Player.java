e212eb19406392ad2bd0c17eca81c31d18e78a3d
novalex98 <AlexanderNovok98@gmail.com>
2015-09-02 17:38:38 -0700

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,29 @@
+public class Player {
+  public int vp;
+  public int target;
+  public Ship myShip;
+  public String name;
+  
+  public Player(int target, Ship ship, String name) {
+    this.target = target;
+    myShip = ship;
+    vp = 0;
+    this.name = name;
+  }
+  
+  public void getVp(int gain) {
+    vp = vp + gain;
+  }
+  
+  public boolean hasWon() {
+    return (vp >= target);
+  }
+  
+  public void changeShip(Ship newShip) {
+    myShip = ship;
+  }
+  
+  public string toString() {
+    return name;
+  }
+}
