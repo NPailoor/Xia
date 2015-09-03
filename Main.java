f34382bbfca46508cc3522887cf7eb5141da5d5d
novalex98 <AlexanderNovok98@gmail.com>
2015-09-02 19:12:07 -0700

    Update Main.java
    fixed a couple of errors

--- Main.java
+++ Main.java
@@ -3,18 +3,18 @@ import java.util.*; // I think this is where collections is?
 public class Main {
   Player[] turnQueue;
   Board myBoard;
-  Tile[] tileStack;
+  ArrayList<Tile> tileStack;
   int turnNum;
   boolean gameOver;
   Player winner;
   String title;
   int target;
   
-  public Main(Player[] turnQueue, Board myBoard, Tile[] tileStack, String title, int target) {
+  public Main(Player[] turnQueue, Board myBoard, ArrayList<Tile> stack, String title, int target) {
     this.turnQueue = turnQueue;
     this.myBoard = myBoard;
-    this.tileStack = tileStack;
-    Collections.shuffle(Arrays.asList(tileStack)); // this should shuffle the tileStack.
+    tileStack = stack;
+    Collections.shuffle(tileStack); // this should shuffle the tileStack.
     this.title = title;
     this.target = target;
     turnNum = 0;
