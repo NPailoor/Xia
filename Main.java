2723e091cff85d412a13d791378ef32df021817f
novalex98 <AlexanderNovok98@gmail.com>
2015-09-02 17:53:33 -0700

    Update Main.java
    Players don't need to know if they've won. Nor do they need to know the target number. Only the Main needs to know these things

--- Main.java
+++ Main.java
@@ -6,23 +6,29 @@ public class Main {
   boolean gameOver;
   Player winner;
   String title;
+  int target;
   
-  public Main(Player[] turnQueue, Board myBoard, Tile[] tileStack, String title) {
+  public Main(Player[] turnQueue, Board myBoard, Tile[] tileStack, String title, int target) {
     this.turnQueue = turnQueue;
     this.myBoard = myBoard;
     this.tileStack = tileStack;
     this.title = title;
+    this.target = target;
     turnNum = 0;
     gameOver = false;
     while(!gameOver) {
       gameOver = this.advanceTurn();
+      if (gameOver) {
+        winner = turnQueue[n-1];
+      }
     }
+    
   }
   
   private boolean advanceTurn() {
     turnPlayer = turnQueue(n);
     turnPlayer.takeTurn(); // not sure about this method yet
     n++;
-    return turnPlayer.hasWon();
+    return (player.vp > target));
   }
 }
